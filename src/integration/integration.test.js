54a689aa1888ef24bacb798455a46086792f2999
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-11-07 20:12:04 -0500

    Example integration setup with two hosts and a simple test

--- src/integration/integration.test.js
+++ src/integration/integration.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
 
 describe('Integration setup', () => {
-  it('should have two servers', () => {
-    const url1 = 'http://localhost:4001/';
-    const url2 = 'http://localhost:4002/';
-    const req1 = request(url1).get('/');
-    const req2 = request(url2).get('/');
+  it('should have two servers', async () => {
+    const url1 = 'http://localhost:4040';
+    const url2 = 'http://localhost:4041';
+    const req1 = await request(url1).get('/');
+    const req2 = await request(url2).get('/');
     expect(req1.statusCode).toEqual(200);
     expect(req2.statusCode).toEqual(200);
   });
